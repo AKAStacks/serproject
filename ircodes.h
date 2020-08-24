60608ac3e57b5fbc8d96e78870c4942bbd1427c4
AKAStacks <denniszarger@gmail.com>
2020-08-24 09:57:11 -0500

    Finish command chain shtuff, func hashmap v enum?

--- ircodes.h
+++ ircodes.h
@@ -4,4 +4,107 @@
 // uint16_t rawData[71] = {8910, 4474,  536, 592,  484, 1746,  508, 1724,  508, 594,  508, 594,  510, 592,  510, 542,  560, 1722,  510, 1720,  510, 592,  510, 1720,  510, 564,  540, 588,  512, 564,  538, 540,  560, 540,  562, 1720,  512, 1716,  514, 1716,  514, 1692,  538, 590,  514, 560,  540, 562,  540, 590,  512, 536,  564, 562,  538, 540,  560, 566,  536, 1696,  536, 1668,  564, 1716,  514, 1692,  540, 43984,  8932, 2246,  532};  // NEC 61A0F00F                                                                                                                                                                                                    
 // uint32_t address = 0x586;                                                                                                                                                                                                                     
 // uint32_t command = 0xF;                                                                                                                                                                                                                       
-uint64_t SamsungPowerToggle = 0xE0E040BF;
+const uint64_t SamsungPowerToggle = 0xE0E040BF;
+
+// Let's make it change backlight and brightness
+
+const uint64_t sMenu = 0xE0E058A7;
+const uint64_t sDown = 0xE0E08679;
+const uint64_t sRight = 0xE0E046B9;
+const uint64_t sLeft = 0xE0E0A659;
+const uint64_t sExit = 0xE0E0B44B;
+const uint64_t sEnter = 0xE0E016E9;
+
+// Arrays (reduce then raise)
+uint64_t nightLight[37] = {
+  sMenu,
+  sRight,
+  sDown,
+  sRight,
+
+  sLeft,
+  sLeft,
+  sLeft,
+  sLeft,
+  sLeft,
+  sLeft,
+  sLeft,
+  sLeft,
+  sLeft,
+  sLeft,
+
+  // OK Backlight is UP
+
+  sDown,
+  sDown,
+
+  sLeft,
+  sLeft,
+  sLeft,
+  sLeft,
+  sLeft,
+  sLeft,
+  sLeft,
+  sLeft,
+  sLeft,
+  sLeft,
+  sLeft,
+  sLeft,
+  sLeft,
+  sLeft,
+  sLeft,
+  sLeft,
+  sLeft,
+  sLeft,
+  sLeft,
+  sLeft,
+
+  sExit
+};
+
+
+uint64_t dayLight[37] = {
+  sMenu,
+  sRight,
+  sDown,
+  sRight,
+
+  sRight,
+  sRight,
+  sRight,
+  sRight,
+  sRight,
+  sRight,
+  sRight,
+  sRight,
+  sRight,
+  sRight,
+
+  // OK Backlight is UP
+
+  sDown,
+  sDown,
+
+  sRight,
+  sRight,
+  sRight,
+  sRight,
+  sRight,
+  sRight,
+  sRight,
+  sRight,
+  sRight,
+  sRight,
+  sRight,
+  sRight,
+  sRight,
+  sRight,
+  sRight,
+  sRight,
+  sRight,
+  sRight,
+  sRight,
+  sRight,
+
+  sExit
+};
--- irproject.ino
+++ irproject.ino
@@ -1,15 +1,8 @@
 // IR Project
-// This code is without warrenty and public domain
+// This code is without warranty and public domain
 
 #define _IR_ENABLE_DEFAULT_ false
-#define DECODE_SAMSUNG true
 #define SEND_SAMSUNG true
-
-#include <ESP8266WebServer.h>
-#include <Uri.h>
-#include <ESP8266mDNS.h>
-
-#include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
 
 #include <PolledTimeout.h>
 #include <Wire.h>
@@ -29,106 +22,22 @@ int LED = LED_BUILTIN;
 #define IRLED D5  // GPIO 14 (D5)
 IRsend irsend(IRLED);
 
-ESP8266WebServer server(80);
-
 String inputString = "";
 bool stringComplete = false;
+
+bool inCommandChain = false;
+enum BrightAdjust = { up, down, none };
+BrightAdjust brightAdjust = none;
+int arrayLength = 0;
+int currentCommandStep = 0;
 
 void tick() {
   digitalWrite(LED, !digitalRead(LED));
 }
 
-void announce(String str) {
-  // Serial.println(str);
-}
-
-void configModeCallback (WiFiManager *myWiFiManager) {
-  announce("Entered config mode");
-  //announce(WiFi.softAPIP());
-  //if you used auto generated SSID, print it
-  announce(myWiFiManager->getConfigPortalSSID());
-  //entered config mode, make led toggle faster
-  ticker.attach(0.2, tick);
-}
-
-void handleRoot() {
-  server.send(200, "text/html",
-  "<html>\
-    <body style='background: black; overflow: hidden;'>\
-        <div style='width: 100vw; height: 100vh; display: grid; place-items: center; color: white;'>\
-        Ayyo was good\
-        </br>this post brought to you by @emacs arduino-mode\
-        </br>\
-        <button type='button' onclick='toggleTV();'>Toggle TV</button>\
-        <script>\
-            function toggleTV() {\
-              let xhttp = new XMLHttpRequest();\
-              xhttp.onreadystatechange = function() {\
-                if (this.readyState == 4 && this.status == 200) {\
-                }\
-              };\
-              xhttp.open('POST', 'toggletv', true);\
-              xhttp.send();\
-            };\
-        </script>\
-      </div>\
-     </body>\
-   </html>");
-}
-
-void handleNotFound() {
-  String message = "File Not Found\n\n";
-  message += "URI: ";
-  message += server.uri();
-  message += "\nMethod: ";
-  message += (server.method() == HTTP_GET) ? "GET" : "POST";
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
-  for (uint8_t i = 0; i < server.args(); i++) {
-    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
-  }
-  server.send(404, "text/plain", message);
-}
-
 void toggleTV() {
    irsend.sendSAMSUNG(SamsungPowerToggle);
    // Serial.println("Sending toggle IR signal.");
-}
-
-void initWiFi() {
-  WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP
-  pinMode(LED, OUTPUT);
-  ticker.attach(0.6, tick);
-
-  WiFiManager wm;
-  //reset settings - for testing
-//   wm.resetSettings();
-
-  //set callback that gets called when connecting to previous WiFi fails, and enters Access Point mode
-  wm.setAPCallback(configModeCallback);
-
-  if (!wm.autoConnect()) {
-    // Serial.println("failed to connect and hit timeout");
-    //reset and try again, or maybe put it to deep sleep
-    ESP.restart();
-    delay(1000);
-  }
-
-  // Serial.println("connected...yeey :)");
-  ticker.detach();
-  //keep LED off
-  digitalWrite(LED, HIGH);
-
-  if (MDNS.begin("remoteoff")) {
-    // Serial.println("MDNS responder started");
-  }
-  server.on("/", handleRoot);
-  server.on("/toggletv", toggleTV);
-  server.onNotFound(handleNotFound);
-  server.begin();
-  
-  announce("IR - Connected");
 }
 
 void handleSerial() {
@@ -153,14 +62,48 @@ void handleSerial() {
   }
 }
 
+void doNextCommand(direction) {
+  int arrayLength = 0;
+  switch(direction) {
+    case up:
+      arrayLength = sizeof(dayLight)/sizeof(dayLight[0]);
+      break;
+    case down:
+      arrayLength = sizeof(nightLight)/sizeof(nightLight[0]);
+      break;
+  }
+
+  if
+}
+
+void handleCommandChain() {
+  if (inCommandChain) {
+    if (arrayLength == 0) {
+      switch(brightAdjust) {
+        case up:
+          arrayLength = sizeof(dayLight)/sizeof(dayLight[0]);
+          break;
+        case down:
+          arrayLength = sizeof(nightLight)/sizeof(nightLight[0]);
+          break;
+      }
+    }
+
+    if (currentCommandStep >= arrayLength) {
+      inCommandChain = false;
+
+    }
+
+  }
+}
+
 void setup() {
   Serial.begin(115200);
-  // Serial.println();
-  pinMode(IRLED, OUTPUT); 
+
+  pinMode(IRLED, OUTPUT);
 
   inputString.reserve(200);
 
-  // initWiFi();
   irsend.begin();
 }
 
@@ -170,6 +113,4 @@ void loop() {
 
   handleSerial();
 
-  // server.handleClient();
-  // MDNS.update();
 }
